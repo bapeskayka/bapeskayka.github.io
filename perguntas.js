b1eb0b677d3f7e91a7f0a4c4012eefee8acfca73
bapeskayka <159459041+bapeskayka@users.noreply.github.com>
2024-10-09 11:10:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'brinco',
+    'quanto custa um brinco de ouro?',
+    'custa media 100 reais'
+)
+
+criaCartao(
+    'futebol',
+    'quem ganhou a ultima copa do mundo?',
+    'a Argentina'
+)
+
+criaCartao(
+    'peso',
+    'quanto pesa uma barra olimpica?',
+    'pesa 20 quilos'
+)
+
+criaCartao(
+    'estrangeiro',
+    'qual a nacionalidade de CBUM?',
+    'ele é canadense'
+)
+criaCartao(
+    'lingua inglesa',
+    'como se diz tchau em inglês',
+    'se fala bye'
+)
+
+criaCartao(
+    'futebol',
+    'qual o primeiro ano messi ganhou bola de ouro',
+    '2009'
+
+)
+criaCartao(
+    'academia',
+    'quem ganhou o olimpia ano passado',
+    'cris bumbster'
+
+)
+criaCartao(
+    'jogos',
+    'qual o jogo mais jogado em sala',
+    'Brawl stars'
+
+)
+criaCartao(
+    'estudo',
+    'qual a media para passar de ano',
+    ' a media é 60'
+
+)
+criaCartao(
+    'medicina',
+    'quanto tempo leva para se formar',
+    '6 anos'
+
+)
